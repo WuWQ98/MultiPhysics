adaa249c61c339e129e351b705a384ddb2de6631
WuWQ98 <wuweiqiang98@163.com>
2022-10-07 14:18:48 +0800

    fix

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import webview
 
 if __name__ == '__main__':
-  window = webview.create_window(title="Cable Analytics",url="./react-app/build/index.html",width=1024,height=768)
+  window = webview.create_window(title="MultiPhysics",url="./react-app/build/index.html",width=1024,height=768)
   webview.start(debug=True)
   pass
